545c6d4ac30ff4629f66ef516134db866fbb38e2
cfowl <goose_7@msn.com>
2020-09-20 11:02:18 -0600

    updated h1 and footer CSS for mobile viewing

--- js/dates.js
+++ js/dates.js
@@ -5,10 +5,10 @@ let options = {
 };
 
 // shows current date
-document.getElementById("currentDate").textContent = new Date().toLocaleDateString("en-US", options);
+document.getElementById("currentDate").textContent = "Today: " + new Date().toLocaleDateString("en-US", options);
 
 // shows when document was last modified
-document.getElementById("lastUpdated").textContent = document.lastModified;
+document.getElementById("lastUpdated").textContent = "Last Updated: " + document.lastModified;
 
 // shows current year for copyright
-document.getElementById("currentYear").textContent = new Date().getFullYear();
+document.getElementById("currentYear").innerHTML = "&copy; " + new Date().getFullYear() + " | Chris Fowler | Utah |";
