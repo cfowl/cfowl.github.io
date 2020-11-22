3c59401584f8d4562878a9538dd74977a35eb7c2
cfowl <goose_7@msn.com>
2020-11-21 22:07:17 -0700

    all weather JS moved to one .js file

--- lesson10/js/weather-api.js
+++ lesson10/js/weather-api.js
@@ -1,3 +1,4 @@
+// ** CURRENT WEATHER DATA ** //
 // city ID for preston is 5604473
 const apiURL = "https://api.openweathermap.org/data/2.5/weather?id=5604473&units=imperial&appid=8cc890828b10d4b67c888df88fd63a40";
 
@@ -22,6 +23,32 @@ fetch(apiURL)
     document.getElementById("windchill").innerHTML = windchill;
     document.getElementById("humidity").textContent = jsObject.main.humidity;
     document.getElementById("windspeed").textContent = windspeed;
+
+  });
+
+// ** WEATHER FORECAST DATA ** //
+// city ID for preston is 5604473
+const forecastURL = "https://api.openweathermap.org/data/2.5/forecast?id=5604473&units=imperial&appid=8cc890828b10d4b67c888df88fd63a40";
+
+fetch(forecastURL)
+  .then(response => response.json())
+  .then(jsForecast => {
+      // gets only the forecasts for 18:00:00
+      const forecast = jsForecast.list.filter(i => i.dt_txt.includes("18:00:00"));
+
+      let num = 1;
+      forecast.forEach(i => {
+          const dayOfWeek = new Date(i.dt_txt).toString().substr(0,3);
+          const day = document.getElementById(`day${num++}`);
+          day.getElementsByTagName("h4")[0].textContent = dayOfWeek;
+
+          const icon = day.getElementsByTagName("img")[0];
+          const iconSrc = `https://openweathermap.org/img/w/${i.weather[0].icon}.png`;
+          icon.setAttribute("src", iconSrc);
+
+          day.getElementsByTagName("span")[0].innerHTML = `${i.main.temp.toFixed(1)}&deg; F`;
+
+      })
 
   });
 
